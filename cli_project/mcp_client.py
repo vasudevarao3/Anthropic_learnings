d42e4cb5f541a6e6462a911607484fe624e3db59
vasudevarao3 <kvasudevarao3@gmail.com>
2026-05-04 14:00:58 +0530

    Access Prompts

--- cli_project/mcp_client.py
+++ cli_project/mcp_client.py
@@ -56,11 +56,13 @@ class MCPClient:
 
     async def list_prompts(self) -> list[types.Prompt]:
         # TODO: Return a list of prompts defined by the MCP server
-        return []
+        result = await self.session().list_prompts()
+        return result.prompts 
 
     async def get_prompt(self, prompt_name, args: dict[str, str]):
         # TODO: Get a particular prompt defined by the MCP server
-        return []
+        result = await self.session().get_prompt(prompt_name, args)
+        return result.messages 
 
     async def read_resource(self, uri: str) -> Any:
         # TODO: Read a resource, parse the contents and return it
